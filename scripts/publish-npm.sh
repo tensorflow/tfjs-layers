68d066eb2022473fe3d9bdd960e8a6c69d1054f5
Daniel Smilkov <dsmilkov@gmail.com>
2018-03-16 15:49:12 -0400

    save

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -5,12 +5,16 @@
 # https://opensource.org/licenses/MIT.
 # =============================================================================
 
-# Run this as `yarn publish-npm`.
+# Before you publish a new version:
+# 1) Update the version in package.json
+# 2) Run ./scripts/make-version from the base dir of the project.
+# 3) Commit to github.
+# 4) Run `yarn publish-npm` from the base dir of the project.
 
 set -e
 
 yarn build-npm
-./scripts/make-version
+./scripts/make-version # This is for safety in case you forgot to do 2).
 npm publish
 ./scripts/tag-version
 echo 'Yay! Published a new package to npm.'
