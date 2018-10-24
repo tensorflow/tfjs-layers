42a3073c633f30c9ca4e58c89ee83487d527560c
Shanqing Cai <cais@google.com>
2018-10-24 11:00:06 -0400

    Fix code snippet

--- src/engine/input_layer.ts
+++ src/engine/input_layer.ts
@@ -56,19 +56,21 @@ export interface InputLayerConfig {
  * model1.add(tf.layers.dense({inputShape: [4], units: 3, activation: 'relu'}));
  * model1.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
  * model1.summary();
+ * model1.preidct(tf.zeros([1, 4])).print();
  *
  * // Construct another model, reusing the second layer of `model1` while
  * // not using the first layer of `model1`. Note that you cannot add the second
  * // layer of `model` directly as the first layer of the new sequential model,
- * // because doing so will lead to an error that says the layer is not an input
- * // layer. Instead, you need to create an `inputLayer` and add it to the
- * // new sequential model before adding the reused layer.
+ * // because doing so will lead to an error related to the fact that the layer
+ * // is not an input layer. Instead, you need to create an `inputLayer` and add
+ * // it to the new sequential model before adding the reused layer.
  * const model2 = tf.sequential();
  * // Use an inputShape that matches the input shape of `model1`'s second
  * // layer.
  * model2.add(tf.layers.inputLayer({inputShape: [3]}));
- * model2.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
+ * model2.add(model1.layers[1]);
  * model2.summary();
+ * model2.preidct(tf.zeros([1, 3])).print();
  * ```
  */
 export class InputLayer extends Layer {
