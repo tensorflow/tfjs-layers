ae6f2ab99fbb02660c7180b5205889f8077f29e3
Daniel Smilkov <dsmilkov@gmail.com>
2018-10-26 13:51:26 -0400

    save

---
+++ scripts/test-travis.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 Google LLC
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+# =============================================================================
+
+set -e
+
+# If this is nightly, use tfjs-core at master.
+if [ "$TRAVIS_EVENT_TYPE" = cron ]
+then
+  rm -rf tfjs-core
+  git clone https://github.com/tensorflow/tfjs-core.git --depth=5
+  cd tfjs-core
+  yarn
+  yarn build
+  yarn publish-local
+  cd ..
+  yarn link-local '@tensorflow/tfjs-core'
+fi
+
+# Regular testing.
+yarn build
+yarn lint
+yarn test-travis
