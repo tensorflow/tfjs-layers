695f6f5804633a93fa233c4b31587b2566f5632c
David Soergel <soergel@google.com>
2019-01-24 15:30:30 -0500

    Docstring for stringLiteralArray

--- src/keras_format/utils.ts
+++ src/keras_format/utils.ts
@@ -8,7 +8,21 @@
  * =============================================================================
  */
 
-// https://stackoverflow.com/questions/52085454/typescript-define-a-union-type-from-an-array-of-strings/52085658
+/**
+ * Infers a string union type from an array of string literals, and returns
+ * the array as an array of that type.
+ *
+ * For instance:
+ *
+ * ```
+ * const fruits = stringLiteralArray(['apple', 'banana', 'orange']);
+ * type Fruit = typeof activationOptions[number];
+ * ```
+ *
+ * now `Fruit` is the union type `'apple'|'banana'|'orange'`.
+ *
+ * https://stackoverflow.com/questions/52085454/typescript-define-a-union-type-from-an-array-of-strings/52085658
+ */
 export function stringLiteralArray<T extends string>(a: T[]) {
   return a;
 }
