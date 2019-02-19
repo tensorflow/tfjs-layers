08dc782f2c506e89ee901d74c4bfe02c8a869d16
Shanqing Cai <cais@google.com>
2019-02-19 11:06:28 -0500

    Adjust test name

--- src/layers/pooling_test.ts
+++ src/layers/pooling_test.ts
@@ -234,7 +234,7 @@ describeMathCPUAndGPU('Pooling Layers 1D: Tensor', () => {
     }
   }
 
-  it('Tolerates poolSize and strides passed as number arrays', () => {
+  it('Handles poolSize and strides passed as number arrays', () => {
     const model = tfl.sequential();
     model.add(tfl.layers.maxPool1d(
         // tslint:disable-next-line:no-any
