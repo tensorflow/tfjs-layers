6a06f55eec1976cb27c7f820ed42fd3a63a9d838
Daniel Smilkov <dsmilkov@gmail.com>
2019-04-16 16:46:47 -0400

    save

--- scripts/tfjs2keras-py.sh
+++ scripts/tfjs2keras-py.sh
@@ -10,7 +10,52 @@
 set -e
 
 cd integration_tests/tfjs2keras
-./run-test.sh --stable
-./run-test.sh --stable --tfkeras
-./run-test.sh --dev --tfkeras
-rm -r "test-data"
+DEV_VERSION=""
+TFJS2KERAS_TEST_USING_TF_KERAS=0
+while [[ ! -z "$1" ]]; do
+  if [[ "$1" == "--stable" ]]; then
+    DEV_VERSION="stable"
+  elif [[ "$1" == "--dev" ]]; then
+    DEV_VERSION="dev"
+  elif [[ "$1" == "--tfkeras" ]]; then
+    TFJS2KERAS_TEST_USING_TF_KERAS=1
+  else
+    echo "ERROR: Unrecognized command-line flag $1"
+    exit 1
+  fi
+  shift
+done
+
+echo "DEV_VERSION: ${DEV_VERSION}"
+echo "TFJS2KERAS_TEST_USING_TF_KERAS: ${TFJS2KERAS_TEST_USING_TF_KERAS}"
+
+if [[ -z "${DEV_VERSION}" ]]; then
+  echo "Must specify one of --stable and --dev."
+  exit 1
+fi
+
+if [[ "${DEV_VERSION}" == "dev" &&
+      "${TFJS2KERAS_TEST_USING_TF_KERAS}" == "0" ]]; then
+  echo "--dev && keras-team/keras is not a valid combination."
+  echo "Use --dev and --tfkeras together."
+  exit 1
+fi
+
+VENV_DIR="$(mktemp -d)_venv"
+echo "Creating virtualenv at ${VENV_DIR} ..."
+virtualenv "${VENV_DIR}"
+source "${VENV_DIR}/bin/activate"
+
+if [[ "${DEV_VERSION}" == "stable" ]]; then
+  pip install -r requirements-stable.txt
+else
+  pip install -r requirements-dev.txt
+fi
+
+export TFJS2KERAS_TEST_USING_TF_KERAS="${TFJS2KERAS_TEST_USING_TF_KERAS}"
+
+python tfjs2keras_test.py
+
+# Clean up virtualenv directory.
+rm -rf "${VENV_DIR}"
+cd ../..
