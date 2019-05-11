0a4f6884356195de99b88783fe9141f990c9b6a2
Jactus <dw1920@nyu.edu>
2019-05-11 10:58:28 -0400

    Register LogSoftmax activation function

--- src/activations.ts
+++ src/activations.ts
@@ -10,10 +10,10 @@
 
 // Layer activation functions
 import * as tfc from '@tensorflow/tfjs-core';
-import {serialization, Tensor, tidy} from '@tensorflow/tfjs-core';
+import { serialization, Tensor, tidy } from '@tensorflow/tfjs-core';
 import * as K from './backend/tfjs_backend';
-import {ActivationIdentifier} from './keras_format/activation_config';
-import {deserializeKerasObject} from './utils/generic_utils';
+import { ActivationIdentifier } from './keras_format/activation_config';
+import { deserializeKerasObject } from './utils/generic_utils';
 
 /**
  * Base class for Activations.
@@ -183,20 +183,44 @@ export class Softmax extends Activation {
 }
 serialization.registerClass(Softmax);
 
+/**
+ * Log softmax activation function
+ */
+export class LogSoftmax extends Activation {
+  /** @nocollapse */
+  static readonly className = 'logsoftmax';
+  /**
+   * Calculate the activation function.
+   *
+   * @param x Tensor.
+   * @param axis Integer, axis along which the softmax normalization is applied.
+   * Invalid if < 2, as softmax across 1 (the batch dimension) is assumed to be
+   * an error.
+   *
+   * @returns a Tensor of the same shape as x
+   *
+   * @throws ValueError: In case `dim(x) < 2`.
+   */
+  apply(x: Tensor, axis: number = (-1)): Tensor {
+    return tfc.logsoftmax(x, axis);
+  }
+}
+serialization.registerClass(LogSoftmax);
+
 export function serializeActivation(activation: Activation): string {
   return activation.getClassName();
 }
 
 export function deserializeActivation(
-    config: serialization.ConfigDict,
-    customObjects: serialization.ConfigDict = {}): Activation {
+  config: serialization.ConfigDict,
+  customObjects: serialization.ConfigDict = {}): Activation {
   return deserializeKerasObject(
-      config, serialization.SerializationMap.getMap().classNameMap,
-      customObjects, 'activation');
+    config, serialization.SerializationMap.getMap().classNameMap,
+    customObjects, 'activation');
 }
 
-export function getActivation(identifier: ActivationIdentifier|
-                              serialization.ConfigDict|Activation): Activation {
+export function getActivation(identifier: ActivationIdentifier |
+  serialization.ConfigDict | Activation): Activation {
   if (identifier == null) {
     const config: serialization.ConfigDict = {};
     config['className'] = 'linear';
