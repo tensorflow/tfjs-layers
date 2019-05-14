8c02e0f4ca49bd292833f8a8c8a0128481cfdbd0
Nikhil Thorat <nsthorat@google.com>
2019-05-14 11:01:14 -0400

    Add testing of snippets (not working)

---
+++ scripts/test_snippets.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+// import * as tfl from '../src/index';
+// import * as tfc from '@tensorflow/tfjs-core';
+import * as util from '@tensorflow/tfjs-core/scripts/test_snippets/util';
+
+// const tf = {
+//   ...tfl,
+//   ...tfc
+// };
+console.log(util);
+// parseAndEvaluateSnippets(tf);
